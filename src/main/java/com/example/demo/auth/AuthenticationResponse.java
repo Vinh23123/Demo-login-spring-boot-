ae1ca593478690c48d00d82cd960b0449d80c461
Vinh <Vinhhoangvh11a@gmail.com>
2023-04-05 23:51:57 +0700

    update

---
+++ src/main/java/com/example/demo/auth/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.example.demo.auth;
+
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private String token;
+
+}
